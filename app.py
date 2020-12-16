415a338f376740127366248fc6618293b5d4cd5c
Tobias Bück <youremail@yourdomain.com>
2020-12-16 16:41:57 +0100

    email signup

--- app.py
+++ app.py
@@ -7,16 +7,25 @@ from databaseQueries import DatabaseConnection
 app = Flask(__name__)
 
 
-@app.route('/')
+def get_columns():
+    #column_names = [Column("Bank", False, ""), Column("Beschreibung", False, ""), Column("Preis im Jahr", True, "price_per_year")]
+    pass
+
+@app.route('/comparison')
 def index():
     column = request.args.get('sort_by', None)
+    is_ascending = request.args.get('sort_type', None)
     all_banks = DatabaseConnection.get_all_bank_objects()
-    column_names = [Column("Bank", False, ""), Column("Beschreibung", False, ""), Column("Preis im Jahr", True, "price_per_year")]
     print(column)
     if column is not None and column == "price_per_year":
         all_banks = BankObjectSorter().sort_by_price(all_banks)
 
-    return render_template("index.html", banks=all_banks, column_names=column_names)
+    return render_template("index.html", banks=all_banks)
+
+
+@app.route('/')
+def email_signup():
+    return render_template("email_signup.html")
 
 
 if __name__ == '__main__':
--- column.py
+++ column.py
@@ -6,3 +6,5 @@ class Column:
     name: str
     can_be_sorted: bool
     sort_name: str
+    currently_sorted: bool
+    ascending_sorted: bool
